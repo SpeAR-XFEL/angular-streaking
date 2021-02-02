bf2b887eef31cfe1e3df3ad7e6691c2ef0af0068
Lars Funke <lars.funke@tu-dortmund.de>
2021-02-02 17:36:55 +0100

    added electrons class

---
+++ streaking/components/electrons.py
@@ -0,0 +1,33 @@
+import numpy as np
+import scipy.constants as const
+
+
+class Electrons:
+    """
+    Holds a set of electrons. Maybe this should become a `Particles` class at some point?
+    """
+
+    def __init__(self, r, p, Ekin=None, t0=None):
+        """
+        Parameters
+        ----------
+        r : (N, 3) array_like
+            Position vectors in meter.
+        p : (N, 3) array_like
+            Momentum vectors in kilogram meter per second.
+        Ekin : (N) array_like, optional
+            If given, p is taken as unit vector of momentum.
+        t0 : (N) array_like, optional
+            Optional birth time. If not given, every electron is born at t0 = 0.
+        """
+        self.r = r
+        if Ekin is None:
+            self.p = p
+        else:
+            pmag = np.sqrt(2 * const.m_e * const.c ** 2 * Ekin + Ekin ** 2) / const.c
+            self.p = pmag * p / np.linalg.norm(p, axis=-1)
+
+        if t0 is None:
+            self.t0 = np.zeros(self.r.shape[0])
+        else:
+            self.t0 = t0
