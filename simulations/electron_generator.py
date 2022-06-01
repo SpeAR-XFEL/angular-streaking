cec161cc5feb26e71a55acbab4e128c5672d53c2
Lars Funke <lars.funke@tu-dortmund.de>
2022-06-01 11:33:46 +0200

    New detector type + changes for june beam time

--- simulations/electron_generator.py
+++ simulations/electron_generator.py
@@ -9,21 +9,30 @@ def pdf(ϑ, φ, cone_angle):
     return ϑ < cone_angle
 
 
-if __name__ == "__main__":
-    n = int(1e5)
-    focus = 2e-5
-    cone_angle = np.deg2rad(6.4)  # np.deg2rad(1.946572802)
+def generate_electrons(energy_start, energy_end, energy_step):
+    n = int(5e4)
+    focus = 1e-5
+    cone_angle = np.deg2rad(6.7)  # np.deg2rad(1.946572802)
     # E = np.random.uniform(50, 150, n) * const.e
-    energy_steps = np.arange(1450, 1550, 1)
+    # energy_steps = np.arange(1450, 1550, 1)
+    energy_steps = np.arange(energy_start, energy_end + energy_step, energy_step)
+    print(energy_steps)
     E = np.repeat(energy_steps, n // len(energy_steps)) * const.e
     px, py, pz = rejection_sampling_spherical(pdf, n, params=(cone_angle,))
     r = np.random.multivariate_normal((0, 0, 0), focus**2 * np.eye(3), n)
+    r[:, 2] = np.random.uniform(-3.5e-3, 3.5e-3, n)
+
     p = np.vstack((pz, py, px)).T
     electrons = ClassicalElectrons(r, p, E, 0)
-    electrons.cst_export('simulations/build/cst_6.4deg_+x_1450eV_1550eV_1eV_100k.pid')
-    print(electrons.Ekin().mean() / const.e)
+    electrons.cst_export(f'simulations/build/cst_6.7deg_+x_{energy_start}eV_{energy_end}eV_{energy_step}eV_{n//1000}k_zspread.pid')
 
-    ax = plt.figure().add_subplot(projection='3d')
-    ax.quiver(*electrons[:100].r.T, *electrons[:100].p.T, length=1e-3, normalize=True, alpha=0.2)
-    ax.set(xlabel='x', ylabel='y', zlabel='z', xlim=(-1e-3, 1e-3), ylim=(-1e-3, 1e-3), zlim=(-1e-3, 1e-3))
+    # ax = plt.figure().add_subplot(projection='3d')
+    # ax.quiver(*electrons[:100].r.T, *electrons[:100].p.T, length=1e-3, normalize=True, alpha=0.2)
+    # ax.set(xlabel='x', ylabel='y', zlabel='z', xlim=(-1e-3, 1e-3), ylim=(-1e-3, 1e-3), zlim=(-1e-3, 1e-3))
     # plt.show()
+
+
+generate_electrons(102, 500, 2)
+#if __name__ == '__main__':
+#    for i in range(1002, 4002, 200):
+#        generate_electrons(i, i + 200 - 2, 2)
--- streaking/detectors.py
+++ streaking/detectors.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.constants as const
 from streaking.conversions import cartesian_to_spherical
-
+from scipy.spatial.transform import Rotation as R
 
 def _cylinder_intersection(electrons, radius, origin=(0, 0, 0), rotation=None):
     origin = np.asarray(origin)[:, None]
@@ -26,7 +26,7 @@ def _cylinder_intersection(electrons, radius, origin=(0, 0, 0), rotation=None):
     return *(cartesian_to_spherical(*intersection_point.T)[(0, 2), :]), intersection_point[:, 2]
 
 
-def _sphere_intersection(electrons, radius, origin=(0, 0, 0), rotation=None):
+def _sphere_intersection(electrons, radius, origin=(0, 0, 0), rotation=None, cartesian=False):
     """
     Calculates where the electron trajectories intersect the sphere of radius `radius` in spherical coordinates.
     Uniform motion is assumed.
@@ -54,7 +54,10 @@ def _sphere_intersection(electrons, radius, origin=(0, 0, 0), rotation=None):
     xpx = x * px + y * py + z * pz
     d = (-2 * xpx + np.sqrt(4 * xpx ** 2 - 4 * psq * (rsq - radius ** 2))) / (2 * psq)
     intersection_point = er + d[:, None] * ep
-    return cartesian_to_spherical(*intersection_point.T)
+    if cartesian:
+        return intersection_point
+    else:
+        return cartesian_to_spherical(*intersection_point.T)
 
 
 def cylindrical_energy_resolved(
@@ -124,7 +127,7 @@ def constant_polar_angle_ring(
         )
 
     return np.histogram2d(
-        (phi[mask] + np.pi / 2) % (2 * np.pi), v, bins=(phibins, variable_bins)
+        (phi[mask]) % (2 * np.pi), v, bins=(phibins, variable_bins)
     )
 
 
@@ -134,4 +137,38 @@ def energy_integrated_4pi(electrons, polar_bins, azimuth_bins, radius, origin):
     b1 = np.linspace(0, 2 * np.pi, azimuth_bins + 1)
     b2 = np.arcsin(np.linspace(-1, 1, polar_bins + 1)) + np.pi / 2
 
-    return np.histogram2d((phi + np.pi / 2) % (2 * np.pi), theta, bins=(b1, b2))
+    return np.histogram2d((phi) % (2 * np.pi), theta, bins=(b1, b2))
+
+
+def flight_tubes(electrons, angles, tip_distance, tip_diameter, mcp_distance, mcp_diameter, variable, variable_bins, variable_quantile=0.01):
+    tip = _sphere_intersection(electrons, tip_distance, cartesian=True)
+    mcp = _sphere_intersection(electrons, mcp_distance, cartesian=True)
+
+    # Rotate around origin such that detectors lie in polar / z direction
+    rot = R.from_euler('YZ', np.stack((np.full_like(angles, np.pi / 2), angles)).T)
+    tip = np.einsum('ijk,lk->jil', rot.as_matrix(), tip)
+    mcp = np.einsum('ijk,lk->jil', rot.as_matrix(), mcp)
+    _, tip_theta, _ = cartesian_to_spherical(*tip)
+    _, mcp_theta, _ = cartesian_to_spherical(*mcp)
+    tip_mask = tip_theta < np.arctan(0.5 * tip_diameter / tip_distance)
+    mcp_mask = mcp_theta < np.arctan(0.5 * mcp_diameter / mcp_distance)
+    comp_mask = tip_mask & mcp_mask
+
+    if variable == "kinetic energy":
+        v = electrons.Ekin() / const.e
+    elif variable == "momentum":
+        v = 3e25 * np.linalg.norm(electrons.p, axis=1)
+    else:
+        raise ValueError(f"Unsupported variable: {variable}")
+
+    # If only bin count is given: Define bins using quantile
+    if np.ndim(variable_bins) == 0:
+        variable_bins = np.linspace(
+            *np.quantile(v, (variable_quantile / 2, 1 - variable_quantile / 2)),
+            variable_bins + 1,
+        )
+    spectra = [np.histogram(v[mask], bins=variable_bins)[0] for mask in comp_mask]
+    #print(tip_mask.sum(), mcp_mask.sum(), comp_mask.sum())
+    # print(tip_theta)
+    
+    return np.array(spectra), np.linspace(0, 2 * np.pi, len(angles) + 1), variable_bins
